77670d3aabfcff2e6d7ed0c73bb5ce7bc76b72d5
Sid-26 <siddhantdas262@gmail.com>
2023-04-06 21:11:06 -0400

    this is messed up

--- src/main/java/com/example/webchatserver/ChatServer.java
+++ src/main/java/com/example/webchatserver/ChatServer.java
@@ -19,8 +19,8 @@ import java.util.List;
 public class ChatServer {
 
     // contains a static List of ChatRoom used to control the existing rooms and their users
-    static HashMap<String, ChatRoom> rooms = new HashMap<>(); // indexed by string and get Chatroom
-
+//    static HashMap<String, ChatRoom> rooms = new HashMap<>(); // indexed by string and get Chatroom
+//    static HashMap<ChatRoom, String> users = new HashMap<>();
 
 
     // you may add other attributes as you see fit
@@ -32,8 +32,11 @@ public class ChatServer {
 //        if (!rooms.containsKey(roomID)) { // created
 //            ChatRoom chatRoom = new ChatRoom(roomID,session.getId());
 //            rooms.put(roomID,chatRoom);
-//        } else {
-//            rooms.get(roomID).setUserName(session.getId(),);
+//            users.put(chatRoom,session.getId());
+//            users.get(rooms.get(roomID));
+//        }
+//        else {
+//            rooms.get(roomID).setUserName(session.getId(), session.getId());
 //        }
         session.getBasicRemote().sendText("Welcome to chat room: " + roomID);
 //        accessing the roomID parameter
@@ -52,17 +55,18 @@ public class ChatServer {
     @OnMessage
     public void handleMessage(String comm, Session session) throws IOException, EncodeException {
 //        example getting unique userID that sent this message
+//        String roomId = rooms.get().getCode();
         String userId = session.getId();
         JSONObject jsonMsg = new JSONObject(comm);
         // {"room": "123ABC", "type": "chat", "msg": "hi"}
-        String room = jsonMsg.get("room").toString();
+        // String room = jsonMsg.get("room").toString();
         String type = jsonMsg.get("type").toString();
         String message = jsonMsg.get("msg").toString();
-
-        if (type.equals("create")) {
-            ChatRoom chatRoom = new ChatRoom(room,session.getId());
-            rooms.put(room,chatRoom);
-        }
+        session.getBasicRemote().sendText("Welcome to chat room: " + roomID);
+//        if (type.equals("create")) {
+//            ChatRoom chatRoom = new ChatRoom(room,session.getId());
+//            rooms.put(room,chatRoom);
+//        }
 //        Example conversion of json messages from the client
         //        JSONObject jsonmsg = new JSONObject(comm);
 //        String val1 = (String) jsonmsg.get("attribute1");
